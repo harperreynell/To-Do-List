ba0ddfb9f851c46a8494ef2d436ef5d23e480da2
Harper Reynell <sentinel.whoami@gmail.com>
2023-11-20 13:55:55 +0200

    added web application

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 let items = document.getElementsByTagName("li")
 
-
+console.log(items)
 for (let i = 0; i < items.length; i++) {
     items[i].addEventListener("click", () => {
         items[i].classList.toggle("done")
--- web/web.go
+++ web/web.go
@@ -1,30 +1,16 @@
 package web
 
 import (
+	"encoding/json"
+	"fmt"
 	"html/template"
 	"net/http"
 	"os"
-	l "todolist/listHandling"
 )
 
-var tpl = template.Must(template.ParseFiles("html/index.html"))
-
 type Todo struct {
-	Item string
-	Done bool
-}
-
-func newTodo(task string, status string) Todo {
-	if status == "closed" {
-		return Todo{
-			Item: task,
-			Done: true}
-	}
-
-	return Todo{
-		Item: task,
-		Done: false,
-	}
+	Item string `json:"item"`
+	Done bool   `json:"done"`
 }
 
 type PageData struct {
@@ -32,12 +18,72 @@ type PageData struct {
 	Todos []Todo
 }
 
-func todo(w http.ResponseWriter, r *http.Request) {
-	var Todos []Todo
-	todoList := l.ReadJsonFromFile(1)
-	for i := 0; i < len(todoList); i++ {
-		Todos = append(Todos, newTodo(todoList[i].Todo, todoList[i].Done))
+var (
+	tpl      = template.Must(template.ParseFiles("html/index.html"))
+	todoList []Todo
+)
+
+func newTodo(task string, status bool) Todo {
+	return Todo{
+		Item: task,
+		Done: status,
 	}
+}
+
+func addTask(w http.ResponseWriter, r *http.Request) {
+	var task string
+	todoList = ReadJsonFromFile()
+	todoList = append(todoList, newTodo(task, false))
+
+	//data := PageData{
+	//	Title: "Your list",
+	//	Todos: todoList,
+	//}
+	//
+	//tpl.Execute(w, data)
+}
+
+func WriteJsonToFile(data []Todo) {
+	file, err := os.Create("tasks.json")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+
+	// Create a JSON encoder
+	encoder := json.NewEncoder(file)
+
+	if err := encoder.Encode(data); err != nil {
+		fmt.Println(err)
+		return
+	}
+}
+
+func ReadJsonFromFile() []Todo {
+	file, err := os.Open("tasks.json")
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+	defer file.Close()
+
+	decoder := json.NewDecoder(file)
+
+	var todoList []Todo
+
+	if err := decoder.Decode(&todoList); err != nil {
+		fmt.Println(err)
+		return nil
+	}
+	return todoList
+
+}
+
+func todo(w http.ResponseWriter, r *http.Request) {
+	//WriteJsonToFile(Todos)
+	Todos := ReadJsonFromFile()
+
 	data := PageData{
 		Title: "Your list",
 		Todos: Todos,
@@ -46,7 +92,42 @@ func todo(w http.ResponseWriter, r *http.Request) {
 	tpl.Execute(w, data)
 }
 
+func formHandler(w http.ResponseWriter, r *http.Request) {
+
+	if err := r.ParseForm(); err != nil {
+		//fmt.Fprintf(w, "Parsing Form err: %v", err)
+		return
+	}
+
+	todoList := ReadJsonFromFile()
+	todoList = append(todoList, newTodo(r.FormValue("task"), false))
+	//fmt.Fprintf(w, "Task: %s", r.FormValue("task"))
+	//printTodos(w, todoList)
+	WriteJsonToFile(todoList)
+	data := PageData{
+		Title: "Your list",
+		Todos: todoList,
+	}
+	tpl.Execute(w, data)
+}
+
+func clearHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		return
+	}
+
+	var todoList []Todo
+	WriteJsonToFile(todoList)
+	data := PageData{
+		Title: "Your list",
+		Todos: todoList,
+	}
+
+	tpl.Execute(w, data)
+}
+
 func Create() {
+	//var task string
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "3000"
@@ -56,7 +137,8 @@ func Create() {
 
 	fs := http.FileServer(http.Dir("assets"))
 	mux.Handle("/assets/", http.StripPrefix("/assets/", fs))
-	mux.HandleFunc("/todo", todo)
-
+	mux.HandleFunc("/", todo)
+	mux.HandleFunc("/form", formHandler)
+	mux.HandleFunc("/clear", clearHandler)
 	http.ListenAndServe(":"+port, mux)
 }
