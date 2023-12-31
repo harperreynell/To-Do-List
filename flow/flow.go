5e4ea451bc50fb658cbf3d5c4b8a476c819f8baa
harperreynell <sentinel.whoami@gmail.com>
2023-12-31 19:08:16 +0200

    updated flow

--- flow/flow.go
+++ flow/flow.go
@@ -2,71 +2,71 @@ package flow
 
 // For console application
 
-//import (
-//	"fmt"
-//	l "todolist/listHandling"
-//)
-//
-//func printMenu() {
-//	fmt.Println("\t1 Print tasks")
-//	fmt.Println("\t2 Add task")
-//	fmt.Println("\t3 Delete task")
-//	fmt.Println("\t4 Change task status")
-//	fmt.Println("\t5 Print help")
-//	fmt.Println("\t99 Exit")
-//}
-//
-//func Flow() {
-//	todos := make([]l.Todos, 0)
-//	var choice, id int = 1, 0
-//	var status string
-//
-//	printMenu()
-//
-//	for choice != 99 {
-//		fmt.Print("Option #> ")
-//		_, err := fmt.Scanln(&choice)
-//		if err != nil {
-//			return
-//		}
-//
-//		switch choice {
-//		case 1:
-//			todos = l.ReadJsonFromFile()
-//			l.PrintTodos(todos)
-//		case 2:
-//			fmt.Println("Type in what you have to do:")
-//			l.ReadTask(&todos)
-//			l.WriteJsonToFile(todos)
-//			fmt.Println("Task was added successfully")
-//		case 3:
-//			todos = l.ReadJsonFromFile()
-//			_, err := fmt.Scanln(&id)
-//			if err != nil || id >= len(todos) {
-//				fmt.Println("No such id")
-//				continue
-//			}
-//			todos = l.DeleteTaskByID(todos, id)
-//			l.WriteJsonToFile(todos)
-//			fmt.Println("Task was deleted successfully")
-//		case 4:
-//			fmt.Println("Type in id of task(id can be found by executing 1 option):")
-//			_, err := fmt.Scanln(&id)
-//			if err != nil || id >= len(todos) {
-//				fmt.Println("No such id")
-//				continue
-//			}
-//
-//			fmt.Println("Type in status [opened/closed]:")
-//			_, _ = fmt.Scanln(&status)
-//			if (status != "opened") && (status != "closed") {
-//				fmt.Println("Unable to set status")
-//			}
-//			l.ChangeStatus(l.TaskByID(todos, id), status)
-//			l.WriteJsonToFile(todos)
-//			fmt.Println("Status changed successfully")
-//		case 5:
-//			printMenu()
-//		}
-//	}
-//}
+import (
+	"fmt"
+	l "todolist/listHandling"
+)
+
+func printMenu() {
+	fmt.Println("\t1 Print tasks")
+	fmt.Println("\t2 Add task")
+	fmt.Println("\t3 Delete task")
+	fmt.Println("\t4 Change task status")
+	fmt.Println("\t5 Print help")
+	fmt.Println("\t99 Exit")
+}
+
+func Flow() {
+	todos := make([]l.Todos, 0)
+	var choice, id int = 1, 0
+	var status string
+
+	printMenu()
+
+	for choice != 99 {
+		fmt.Print("Option #> ")
+		_, err := fmt.Scanln(&choice)
+		if err != nil {
+			return
+		}
+
+		switch choice {
+		case 1:
+			todos = l.ReadJsonFromFileWithoutID()
+			l.PrintTodos(todos)
+		case 2:
+			fmt.Println("Type in what you have to do:")
+			l.ReadTask(&todos)
+			l.WriteJsonToFileWithoutID(todos)
+			fmt.Println("Task was added successfully")
+		case 3:
+			todos = l.ReadJsonFromFileWithoutID()
+			_, err := fmt.Scanln(&id)
+			if err != nil || id >= len(todos) {
+				fmt.Println("No such id")
+				continue
+			}
+			todos = l.DeleteTaskByID(todos, id)
+			l.WriteJsonToFileWithoutID(todos)
+			fmt.Println("Task was deleted successfully")
+		case 4:
+			fmt.Println("Type in id of task(id can be found by executing 1 option):")
+			_, err := fmt.Scanln(&id)
+			if err != nil || id >= len(todos) {
+				fmt.Println("No such id")
+				continue
+			}
+
+			fmt.Println("Type in status [opened/closed]:")
+			_, _ = fmt.Scanln(&status)
+			if (status != "opened") && (status != "closed") {
+				fmt.Println("Unable to set status")
+			}
+			l.ChangeStatus(l.TaskByID(todos, id), status)
+			l.WriteJsonToFileWithoutID(todos)
+			fmt.Println("Status changed successfully")
+		case 5:
+			printMenu()
+		}
+	}
+}
--- listHandling/fileStoringWithoutID.go
+++ listHandling/fileStoringWithoutID.go
@@ -1,1 +1,39 @@
 package listHandling
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+func WriteJsonToFileWithoutID(data []Todos) {
+	file, _ := os.Create("tasks.json")
+	encoder := json.NewEncoder(file)
+	if err := encoder.Encode(data); err != nil {
+		fmt.Println(err)
+		return
+	}
+}
+
+func ReadJsonFromFileWithoutID() []Todos {
+	file, err := os.Open("tasks.json")
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+
+		}
+	}(file)
+
+	decoder := json.NewDecoder(file)
+	var taskList []Todos
+
+	if err := decoder.Decode(&taskList); err != nil {
+		fmt.Println(err)
+		return nil
+	}
+	return taskList
+}
